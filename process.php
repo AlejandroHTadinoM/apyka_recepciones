3f312a69ddec49b6d959b16c0f95facb194997a2
alejandrohtadinom <alejandrohtadinom@gmail.com>
2017-05-29 09:42:27 -0400

    Q&A

--- process.php
+++ process.php
@@ -13,7 +13,7 @@ $mail->CharSet = 'UTF-8';
 $mail->SMTPDebug = 3;
 $mail->setFrom('noreply@dirnovo.com', 'Clicnovo Mailer');
 $mail->Sender='novotecn@gator3249.hostgator.com';
-$mail->addAddress('example@example.com'); // Esta línea, corresponde al mail del cliente
+$mail->addAddress('stelamarygavilan@gmail.com');
 $mail->addBCC('mario@clicnovo.com');
 $mail->addReplyTo($email, $nombre);
 $mail->Subject = "Formulario: " . $asunto;
